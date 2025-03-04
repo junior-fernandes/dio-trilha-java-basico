d9784e1e7e1d882e3cb421b88aba12406fca4126
Junior Braindev <junior@braindev.com.br>
2025-03-04 15:57:12 -0300

    Módulo de tipos e variáveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+
+        String nome = "Junior";
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //VARIAVEIS E CONSTANTES
+        final double VALOR_DE_PI = 3.14;
+
+    }
+
+}
