3f32e113d89721cc76bc0d1d36fc112db6feddaf
Junior Fernandes <ademilsonjnr@gmail.com>
2025-04-19 15:41:15 -0300

    feat: Sintaxe -> Debugging

---
+++ sintaxe/debugging/src/CalculadoraDeMedias.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Junior", "Daia", "Matteo", "Gesica"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+
+    }
+
+}
